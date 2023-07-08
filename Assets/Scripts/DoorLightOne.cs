a046921794cc2b10d73256f864e500f7a3f273cb
MP259689 <MP259689@falmouth.ac.uk>
2023-07-08 14:37:34 +0100

    Added lighting to maze door lights upon collect

--- Assets/Scripts/DoorLightOne.cs
+++ Assets/Scripts/DoorLightOne.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class DoorLightOne : MonoBehaviour
 {
     [SerializeField] GameObject DoorLight1;
+    public GameObject DoorLight1FX;
     [SerializeField] GameObject DoorLight2;
+    public GameObject DoorLight2FX;
     [SerializeField] GameObject DoorLight3;
+    public GameObject DoorLight3FX;
     [SerializeField] GameObject DoorLight4;
+    public GameObject DoorLight4FX;
 
 
     public void Update()
     {
         if (KeyCollection.keysCollected == 5)
             DoorLight1.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+            DoorLight1FX.SetActive(true);
 
         if (KeyCollection.keysCollected == 6)
             DoorLight2.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+             DoorLight2FX.SetActive(true);
 
         if (KeyCollection.keysCollected == 7)
             DoorLight3.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+             DoorLight3FX.SetActive(true);
 
         if (KeyCollection.keysCollected == 8)
             DoorLight4.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+             DoorLight4FX.SetActive(true);
     }
 }
